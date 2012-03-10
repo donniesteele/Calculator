f1e81a94956a6da3fab6e317af3c7bc2b3a8305c
Donald Steele <donaldsteele@Donalds-iMac.(none)>
2012-03-10 17:27:42 -0500

    Initial Commit

---
+++ Calculator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by Donnie Steele on 3/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CalculatorTests/CalculatorTests.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorTests.h
+//  CalculatorTests
+//
+//  Created by Donnie Steele on 3/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CalculatorTests : SenTestCase
+
+@end
